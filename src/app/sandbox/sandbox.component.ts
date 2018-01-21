cf9f486e42c57d9d7a9fde44bc56976ec35f9411
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-01-21 19:34:34 +0300

    Sandbox Variable Types

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -6,27 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sandbox.component.css']
 })
 export class SandboxComponent implements OnInit {
-  name = 'John Doe';
-  age = 35;
-  person = {firstName:'Steve', lastName:'Smith'}
+  name:string = 'John Doe';
+  age:number = 35;
+  hasChildren:boolean = true;
+  city:any = 'Boston';
+  myNumbersArray:number[] = [1,2,3];
+  myStringsArray:string[] = ['hello', 'world'];
+  myAnyArray:any[] = [1,2,'hello'];
+  myTuple: [string, number] = ['hello', 3];
+  unusable: void = undefined;
+  u: undefined = undefined;
+  n: null = null;
 
   constructor() {
-      console.log('Constructor ran...');
-      //this.age = 36;
-      this.hasBirthday();
-      this.hasBirthday();
   }
 
   ngOnInit() {
-  }
-
-
-  hasBirthday(){
-      this.age += 1;
-  }
-
-  showAge(){
-      return this.age;
   }
 
 }
