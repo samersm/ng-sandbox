e9e6eba64e3cf36e501c2840031f759db1e9fbff
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-01-21 21:10:16 +0300

    Using ngCalss

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -6,13 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sandbox.component.css']
 })
 export class SandboxComponent implements OnInit {
-  imageUrl:string = 'http://lorempixel.com/400/200/';
-  isUnchanged:boolean = false;
+  isSpecial = true;
+  canSave = false;
+  currentClasses = {};
 
   constructor() {
+     this.setCurrentClasses();
   }
 
   ngOnInit() {
+  }
+
+  setCurrentClasses(){
+      this.currentClasses = {
+          saveable:this.canSave,
+          special: this.isSpecial
+      }
   }
 
 }
